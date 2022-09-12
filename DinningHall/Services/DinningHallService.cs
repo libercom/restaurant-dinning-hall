10ae7a993c84fce37ac86e77051cb2232c8d7e0c
Vasile <55320717+libercom@users.noreply.github.com>
2022-09-12 22:14:52 +0300

    Finished project init (#1)

---
+++ DinningHall/Services/DinningHallService.cs
@@ -0,0 +1,30 @@
+﻿using DinningHall.Models;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinningHall.Services
+{
+    public class DinningHallService : BackgroundService
+    {
+        private readonly ILogger<DinningHallService> _logger;
+        private readonly string _apiEndpoint;
+
+        public DinningHallService(ILogger<DinningHallService> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _apiEndpoint = configuration.GetSection("KitchenEndpoint").Value;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var client = new HttpClient();
+
+                await Task.Delay(3000);
+
+                await client.PostAsync(_apiEndpoint, new StringContent("", Encoding.UTF8, "application/json"));
+            }
+        }
+    }
+}
